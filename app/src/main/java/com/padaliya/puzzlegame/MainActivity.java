1423aa4d16af03857dbae2ca39232de414f7be43
Kapil Padaliya <39192484+padaliya-Kapil@users.noreply.github.com>
2020-02-02 18:34:41 -0500

    GridLayout

---
+++ app/src/main/java/com/padaliya/puzzlegame/MainActivity.java
@@ -0,0 +1,19 @@
+package com.padaliya.puzzlegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sourceButtonListner(View view)
+    {
+
+    }
+}
